b7590eb997e14085d107126f3cf739efeae5549c
Anh <biebo0101@gmail.com>
2018-03-06 17:31:51 +0700

    xong

---
+++ BT2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+
+namespace BT2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien hoac ip: ");
+            string host = Console.ReadLine();
+
+            IPHostEntry e = Dns.Resolve(host);
+            Console.WriteLine("Hostname: {0}", e.HostName);
+
+            
+
+            // Danh sách điạ chỉ IP
+            foreach (IPAddress i in e.AddressList)
+                Console.WriteLine("\t+ IP: {0}", i);
+        }
+    }
+}
